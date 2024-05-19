1600a6d69aad74afab4dc1e2c2bff7b8e6a1bb88
Dimas Iktiar Pandawi <dimas.pandawi@ottencoffee.co.id>
2024-05-19 10:36:56 +0700

    add migration

---
+++ database/migrations/2024_05_19_103458_update_migration_in_students.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->dropColumn('id_student_class');
+            $table->string('id_school_class')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+};
